65200df11c27dead924c55cbb42680fa79572b26
Noah Kurrack <noahkurrack@gmail.com>
2018-07-22 19:24:42 -0400

    init repository

---
+++ README.md
@@ -0,0 +1,3 @@
+##IFCommissions
+
+###Instructions
---
+++ src/com/noahkurrack/IFCommissions/IFCommissions.java
@@ -0,0 +1,9 @@
+package com.noahkurrack.IFCommissions;
+
+public class IFCommissions {
+
+    public static void main(String[] args) {
+        //entry
+        System.out.println("IFCommissions: initializing");
+    }
+}
